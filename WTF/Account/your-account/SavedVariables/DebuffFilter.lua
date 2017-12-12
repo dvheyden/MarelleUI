429865a0ac1811904a653c7ed2fe6e08b700a038
Marelle <danvdh@gmail.com>
2017-12-12 03:38:16 -0500

    WTF: Update dfilter config

---
+++ WTF/Account/your-account/SavedVariables/DebuffFilter.lua
@@ -0,0 +1,36 @@
+
+DebuffFilter_Config = {
+	["Marelle - Kronos"] = {
+		["debuffs"] = "yes",
+		["all_fbuffs"] = "no",
+		["scale"] = 1.5,
+		["tooltips"] = "yes",
+		["fbuff_orientation"] = "leftright",
+		["debuff_orientation"] = "leftright",
+		["debuff_list"] = {
+			["Fire Vulnerability"] = 1,
+			["Ignite"] = 1,
+			["Curse of the Elements"] = 1,
+		},
+		["all_fdebuffs"] = "no",
+		["fdebuffs"] = "no",
+		["buff_orientation"] = "leftright",
+		["buffs"] = "no",
+		["fbuffs"] = "no",
+		["buff_list"] = {
+			["Adrenaline Rush"] = 1,
+			["Holy Strength"] = 1,
+			["Blade Flurry"] = 1,
+			["Battle Shout"] = 1,
+		},
+		["time_orientation"] = "bottom",
+		["fdebuff_list"] = {
+			["Sunder Armor"] = 1,
+		},
+		["fbuff_list"] = {
+			["Berserker Rage"] = 1,
+		},
+		["fdebuff_orientation"] = "leftright",
+		["count"] = "no",
+	},
+}
