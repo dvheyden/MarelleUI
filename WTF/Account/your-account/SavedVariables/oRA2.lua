898fcbaf438d3fb462ae63103176008409b024ec
Marelle <danvdh@gmail.com>
2017-12-20 22:50:23 -0500

    Addons: Add oRA2

---
+++ WTF/Account/your-account/SavedVariables/oRA2.lua
@@ -0,0 +1,86 @@
+
+oRADB = {
+	["disabled"] = {
+		["Default"] = true,
+	},
+	["namespaces"] = {
+		["Optional/CoolDown"] = {
+			["profiles"] = {
+				["Default"] = {
+					["hidden"] = true,
+				},
+			},
+		},
+		["Participant/Assist"] = {
+			["profiles"] = {
+				["Default"] = {
+					["sound"] = false,
+				},
+			},
+		},
+		["Leader/Invite"] = {
+			["profiles"] = {
+				["Default"] = {
+					["promotes"] = {
+						["areina"] = 1,
+						["theruas"] = 1,
+						["smaskfia"] = 1,
+					},
+					["keyword"] = "+",
+				},
+			},
+		},
+		["Participant/Ready"] = {
+			["profiles"] = {
+				["Default"] = {
+					["sound"] = false,
+				},
+			},
+		},
+	},
+	["disabledModules"] = {
+		["Default"] = {
+			["Participant/CoolDown"] = true,
+			["Participant/Resist"] = true,
+			["Optional/MainTank"] = true,
+			["Participant/Item"] = true,
+			["Optional/CoolDown"] = true,
+			["Participant/Assist"] = true,
+			["Participant/Durability"] = true,
+			["Leader/Item"] = true,
+			["Leader/MainTank"] = true,
+			["Options Menu"] = true,
+			["Leader/Assist"] = true,
+			["Participant/Ready"] = true,
+			["Optional/Resurrection"] = true,
+			["Leader/Invite"] = true,
+			["Optional/Zone"] = true,
+			["Participant/Resurrection"] = true,
+			["Leader/RaidWarn"] = true,
+			["Leader/Ready"] = true,
+			["Participant/MainTank"] = true,
+			["Optional/Version"] = true,
+			["Leader/Resist"] = true,
+			["Participant/RaidWarn"] = true,
+			["Participant/Buff"] = true,
+			["Optional/PlayerTarget"] = true,
+			["Leader/Durability"] = true,
+		},
+	},
+	["profiles"] = {
+		["Default"] = {
+			["useshorthands"] = false,
+			["bartexture"] = "Smooth",
+		},
+	},
+}
+oRAFubarDB = {
+	["profiles"] = {
+		["Default"] = {
+			["detachedTooltip"] = {
+				["fontSizePercent"] = 1,
+			},
+			["hidden"] = true,
+		},
+	},
+}
