76690f73110ead14cfdd75d868e1bf32e195cf1d
Marelle <danvdh@gmail.com>
2017-12-22 03:00:57 -0500

    WTF: Update oRA2 config

--- WTF/Account/your-account/SavedVariables/oRA2.lua
+++ WTF/Account/your-account/SavedVariables/oRA2.lua
@@ -1,9 +1,44 @@
 
 oRADB = {
-	["disabled"] = {
-		["Default"] = true,
+	["disabledModules"] = {
+		["Default"] = {
+			["Participant/CoolDown"] = true,
+			["Participant/Resist"] = true,
+			["Optional/Zone"] = true,
+			["Participant/Item"] = true,
+			["Optional/CoolDown"] = true,
+			["Participant/Assist"] = true,
+			["Participant/Durability"] = true,
+			["Leader/Item"] = true,
+			["Leader/MainTank"] = true,
+			["Options Menu"] = true,
+			["Leader/Assist"] = true,
+			["Participant/Ready"] = true,
+			["Optional/Resurrection"] = true,
+			["Participant/Resurrection"] = true,
+			["Optional/PlayerTarget"] = true,
+			["Participant/Buff"] = true,
+			["Leader/Invite"] = true,
+			["Leader/Ready"] = true,
+			["Participant/MainTank"] = true,
+			["Optional/Version"] = true,
+			["Participant/RaidWarn"] = true,
+			["Leader/Resist"] = true,
+			["Leader/RaidWarn"] = true,
+			["Optional/MainTank"] = true,
+			["Leader/Durability"] = true,
+		},
 	},
 	["namespaces"] = {
+		["Optional/MainTank"] = {
+			["profiles"] = {
+				["Default"] = {
+					["showmtt"] = false,
+					["showmttt"] = false,
+					["showmt"] = false,
+				},
+			},
+		},
 		["Optional/CoolDown"] = {
 			["profiles"] = {
 				["Default"] = {
@@ -36,35 +71,6 @@ oRADB = {
 					["sound"] = false,
 				},
 			},
-		},
-	},
-	["disabledModules"] = {
-		["Default"] = {
-			["Participant/CoolDown"] = true,
-			["Participant/Resist"] = true,
-			["Optional/MainTank"] = true,
-			["Participant/Item"] = true,
-			["Optional/CoolDown"] = true,
-			["Participant/Assist"] = true,
-			["Participant/Durability"] = true,
-			["Leader/Item"] = true,
-			["Leader/MainTank"] = true,
-			["Options Menu"] = true,
-			["Leader/Assist"] = true,
-			["Participant/Ready"] = true,
-			["Optional/Resurrection"] = true,
-			["Leader/Invite"] = true,
-			["Optional/Zone"] = true,
-			["Participant/Resurrection"] = true,
-			["Leader/RaidWarn"] = true,
-			["Leader/Ready"] = true,
-			["Participant/MainTank"] = true,
-			["Optional/Version"] = true,
-			["Leader/Resist"] = true,
-			["Participant/RaidWarn"] = true,
-			["Participant/Buff"] = true,
-			["Optional/PlayerTarget"] = true,
-			["Leader/Durability"] = true,
 		},
 	},
 	["profiles"] = {
