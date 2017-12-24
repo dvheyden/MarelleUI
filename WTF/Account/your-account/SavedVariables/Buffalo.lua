1d7e0df513e075962a12489311231ff4019a3eb7
Marelle <danvdh@gmail.com>
2017-12-24 03:02:36 -0500

    WTF: Add Buffalo config

---
+++ WTF/Account/your-account/SavedVariables/Buffalo.lua
@@ -0,0 +1,32 @@
+
+BuffaloDB = {
+	["profiles"] = {
+		["Default"] = {
+			["xPos"] = {
+				["debuff"] = 1909.167732629966,
+				["buff"] = 1909.167732629966,
+				["weapon"] = 1695.001290262047,
+			},
+			["cols"] = {
+				["buff"] = 12,
+			},
+			["yPos"] = {
+				["debuff"] = 1003.333319336745,
+				["buff"] = 1144.999501388637,
+				["weapon"] = 110.8325751126116,
+			},
+			["flashes"] = {
+				["debuff"] = false,
+				["buff"] = false,
+				["weapon"] = false,
+			},
+			["whiteTimers"] = {
+				["buff"] = false,
+			},
+			["locked"] = true,
+			["verboseTimers"] = {
+				["buff"] = false,
+			},
+		},
+	},
+}
