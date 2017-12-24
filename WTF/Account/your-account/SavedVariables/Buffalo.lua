02388988c89baf26e65deecf0a039df3cabceaaf
Marelle <danvdh@gmail.com>
2017-12-24 03:10:51 -0500

    WTF: Update Buffalo config

--- WTF/Account/your-account/SavedVariables/Buffalo.lua
+++ WTF/Account/your-account/SavedVariables/Buffalo.lua
@@ -10,22 +10,24 @@ BuffaloDB = {
 			["cols"] = {
 				["buff"] = 12,
 			},
-			["yPos"] = {
-				["debuff"] = 1003.333319336745,
-				["buff"] = 1144.999501388637,
-				["weapon"] = 110.8325751126116,
-			},
 			["flashes"] = {
 				["debuff"] = false,
 				["buff"] = false,
 				["weapon"] = false,
 			},
 			["whiteTimers"] = {
+				["debuff"] = false,
 				["buff"] = false,
 			},
 			["locked"] = true,
+			["yPos"] = {
+				["debuff"] = 1003.333319336745,
+				["buff"] = 1144.999501388637,
+				["weapon"] = 110.8325751126116,
+			},
 			["verboseTimers"] = {
-				["buff"] = false,
+				["buff"] = true,
+				["weapon"] = true,
 			},
 		},
 	},
