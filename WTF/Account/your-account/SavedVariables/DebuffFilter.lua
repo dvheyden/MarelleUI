0c59947130c4556a75b15b3823d47e11632da81c
Marelle <danvdh@gmail.com>
2018-06-24 22:19:59 -0400

    WTF: Update dfilter

--- WTF/Account/your-account/SavedVariables/DebuffFilter.lua
+++ WTF/Account/your-account/SavedVariables/DebuffFilter.lua
@@ -1,6 +1,6 @@
 
 DebuffFilter_Config = {
-	["Marelle - Kronos III"] = {
+	["Marelle - Northdale"] = {
 		["debuffs"] = "yes",
 		["all_fbuffs"] = "no",
 		["scale"] = 2,
@@ -45,6 +45,50 @@ DebuffFilter_Config = {
 		["count"] = "no",
 	},
 	["Marelle - Kronos"] = {
+		["debuffs"] = "yes",
+		["all_fbuffs"] = "no",
+		["scale"] = 2,
+		["tooltips"] = "yes",
+		["fbuff_orientation"] = "leftright",
+		["debuff_orientation"] = "leftright",
+		["debuff_list"] = {
+			["Faerie Fire"] = 1,
+			["Ignite"] = 1,
+			["Winter's Chill"] = 1,
+			["Faerie Fire (Feral)"] = 1,
+			["Fire Vulnerability"] = 1,
+			["Sunder Armor"] = 1,
+			["Armor Shatter"] = 1,
+			["Curse of the Elements"] = 1,
+		},
+		["all_fdebuffs"] = "no",
+		["fdebuffs"] = "no",
+		["buff_orientation"] = "rightleft",
+		["buffs"] = "yes",
+		["fbuffs"] = "no",
+		["buff_list"] = {
+			["Ice Barrier"] = 1,
+			["Ephemeral Power"] = 1,
+			["Clearcasting"] = 1,
+			["Arcane Shroud"] = 1,
+			["Mind Quickening"] = 1,
+			["Frost Ward"] = 1,
+			["Ice Block"] = 1,
+			["Combustion"] = 1,
+			["Fire Ward"] = 1,
+			["Arcane Power"] = 1,
+		},
+		["time_orientation"] = "bottom",
+		["fdebuff_list"] = {
+			["Sunder Armor"] = 1,
+		},
+		["fbuff_list"] = {
+			["Berserker Rage"] = 1,
+		},
+		["fdebuff_orientation"] = "leftright",
+		["count"] = "no",
+	},
+		["Marelle - Kronos III"] = {
 		["debuffs"] = "yes",
 		["all_fbuffs"] = "no",
 		["scale"] = 2,
