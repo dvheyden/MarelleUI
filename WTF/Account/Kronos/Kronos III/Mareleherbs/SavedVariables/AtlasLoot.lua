73794f0104249b4e4dbf0201b4a7e188289d732d
dvheyden <44519345+dvheyden@users.noreply.github.com>
2019-03-31 12:36:51 -0400

    update

---
+++ WTF/Account/Kronos/Kronos III/Mareleherbs/SavedVariables/AtlasLoot.lua
@@ -0,0 +1,28 @@
+
+AtlasLootCharDB = {
+	["DefaultTT"] = true,
+	["ItemSyncTT"] = false,
+	["AutoQuery"] = false,
+	["ItemIDs"] = false,
+	["LastBossText"] = "Taragaman the Hungerer",
+	["Opaque"] = false,
+	["SafeLinks"] = true,
+	["SearchResult"] = {
+	},
+	["PartialMatching"] = true,
+	["MinimapButtonRadius"] = 88,
+	["ItemSpam"] = true,
+	["LootlinkTT"] = false,
+	["HidePanel"] = false,
+	["FirstTime"] = false,
+	["EquipCompare"] = false,
+	["AllLinks"] = false,
+	["QuickLooks"] = {
+	},
+	["AtlasLootVersion"] = "40605",
+	["MinimapButtonPosition"] = 308,
+	["WishList"] = {
+	},
+	["MinimapButton"] = false,
+	["LastBoss"] = "RFCTaragaman",
+}
