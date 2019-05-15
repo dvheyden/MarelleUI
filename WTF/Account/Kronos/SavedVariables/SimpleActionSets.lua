ebca515ce04496ed9442795c7b3b57e383255914
dvheyden <44519345+dvheyden@users.noreply.github.com>
2019-05-15 00:24:20 -0400

    Add config

---
+++ WTF/Account/Kronos/SavedVariables/SimpleActionSets.lua
@@ -0,0 +1,197 @@
+
+SAS_Saved = {
+	["BackUp"] = {
+		["s"] = {
+			["Ignitee - Kronos III"] = {
+				[1] = {
+				},
+				[2] = {
+				},
+				[3] = {
+				},
+				[4] = {
+				},
+				[5] = {
+					[12] = "Zandalarian Hero Charm·INV_Jewelry_Necklace_13··?·",
+				},
+				[6] = {
+				},
+				[7] = {
+				},
+				[8] = {
+				},
+				[9] = {
+				},
+				[0] = {
+				},
+			},
+		},
+	},
+	["Ignitee - Kronos III"] = {
+		["s"] = {
+			["Luuga"] = {
+				[1] = {
+				},
+				[2] = {
+					[1] = "Blink·Spell_Arcane_Blink·",
+					[2] = "Ice Barrier·Spell_Ice_Lament·4·",
+					[3] = "Swift Olive Raptor·Ability_Mount_Raptor··18789·",
+					[4] = "Arcane Brilliance·Spell_Holy_ArcaneIntellect·1·",
+					[5] = "Arcane Intellect·Spell_Holy_MagicalSentry·5·",
+					[6] = "Mage Armor·Spell_MageArmor·3·",
+					[7] = "Teleport: Undercity·Spell_Arcane_TeleportUnderCity·",
+					[8] = "Teleport: Orgrimmar·Spell_Arcane_TeleportOrgrimmar·",
+					[9] = "Teleport: Thunder Bluff·Spell_Arcane_TeleportThunderBluff·",
+					[10] = "Dampen Magic·Spell_Nature_AbolishMagic·5·",
+					[11] = "Amplify Magic·Spell_Holy_FlashHeal·4·",
+					[12] = "Tailoring·Trade_Tailoring·",
+				},
+				[3] = {
+					[1] = "Conjure Water·INV_Drink_18·7·",
+					[2] = "Conjure Food·INV_Misc_Food_73CinnamonRoll·7·",
+					[3] = "Conjure Mana Ruby·INV_Misc_Gem_Ruby_01·",
+					[4] = "Conjure Mana Citrine·INV_Misc_Gem_Opal_01·",
+					[5] = "Conjure Mana Jade·INV_Misc_Gem_Emerald_02·",
+					[6] = "Ice Armor·Spell_Frost_FrostArmor02·4·",
+					[7] = "Portal: Undercity·Spell_Arcane_PortalUnderCity·",
+					[8] = "Portal: Orgrimmar·Spell_Arcane_PortalOrgrimmar·",
+					[9] = "Portal: Thunder Bluff·Spell_Arcane_PortalThunderBluff·",
+					[10] = "Detect Magic·Spell_Holy_Dizzy·",
+					[11] = "Remove Lesser Curse·Spell_Nature_RemoveCurse·",
+					[12] = "Slow Fall·Spell_Magic_FeatherFall·",
+				},
+				[4] = {
+					[1] = "Fire Ward·Spell_Fire_FireArmor·5·",
+					[2] = "Frost Ward·Spell_Frost_FrostWard·4·",
+					[3] = "Mana Jade·INV_Misc_Gem_Emerald_02··?·",
+					[4] = "Mana Citrine·INV_Misc_Gem_Opal_01··?·",
+					[5] = "Mana Ruby·INV_Misc_Gem_Ruby_01··8008·",
+					[6] = "Evocation·Spell_Nature_Purge·",
+					[8] = "Bloodcaller·INV_Sword_18··?·",
+					[9] = "Swift Olive Raptor·Ability_Mount_Raptor··18789·",
+					[10] = "Demonic Rune·INV_Misc_Rune_04··?·",
+					[11] = "Major Mana Potion·INV_Potion_76··?·",
+					[12] = "Heavy Runecloth Bandage·INV_Misc_Bandage_12··14530·",
+				},
+				[5] = {
+					[1] = "Arcane Explosion·Spell_Nature_WispSplode·1·",
+					[2] = "Arcane Explosion·Spell_Nature_WispSplode·6·",
+					[3] = "Counterspell·Spell_Frost_IceShock·",
+					[4] = "Blizzard·Spell_Frost_IceStorm·6·",
+					[5] = "Blizzard·Spell_Frost_IceStorm·1·",
+					[6] = "Ice Block·Spell_Frost_Frost·",
+					[7] = "Cold Snap·Spell_Frost_WizardMark·",
+					[9] = "Mana Shield·Spell_Shadow_DetectLesserInvisibility·6·",
+					[10] = "Polymorph·Spell_Nature_Polymorph·4·",
+					[11] = "Talisman of Ephemeral Power·INV_Misc_StoneTablet_11··18820·",
+					[12] = "Zandalarian Hero Charm·INV_Jewelry_Necklace_13··?·",
+				},
+				[6] = {
+				},
+				[7] = {
+				},
+				[8] = {
+				},
+				[9] = {
+				},
+				[0] = {
+					[1] = "Shoot·INV_Wand_09·",
+					[2] = "Frostbolt·Spell_Frost_FrostBolt02·11·",
+					[3] = "Fire Blast·Spell_Fire_Fireball·7·",
+					[4] = "Cone of Cold·Spell_Frost_Glacier·5·",
+					[5] = "Frost Nova·Spell_Frost_FrostNova·1·",
+					[6] = "Arcane Missiles·Spell_Nature_StarFall·8·",
+					[7] = "Fireball·Spell_Fire_FlameBolt·12·",
+					[8] = "Flamestrike·Spell_Fire_SelfDestruct·6·",
+					[9] = "Scorch·Spell_Fire_SoulBurn·7·",
+					[10] = "Frostbolt·Spell_Frost_FrostBolt02·2·",
+					[11] = "Conjured Crystal Water·INV_Drink_18··8079·",
+					[12] = "Conjured Cinnamon Roll·INV_Misc_Food_73CinnamonRoll··?·",
+				},
+			},
+			["Marelle"] = {
+				[1] = {
+				},
+				[2] = {
+					[1] = "Teleport: Orgrimmar·Spell_Arcane_TeleportOrgrimmar·",
+					[2] = "Conjured Cinnamon Roll·INV_Misc_Food_73CinnamonRoll··22895·",
+					[3] = "Swift Olive Raptor·Ability_Mount_Raptor··18789·",
+					[4] = "Arcane Brilliance·Spell_Holy_ArcaneIntellect·1·",
+					[5] = "Arcane Intellect·Spell_Holy_MagicalSentry·5·",
+					[6] = "Mage Armor·Spell_MageArmor·3·",
+					[7] = "Teleport: Undercity·Spell_Arcane_TeleportUnderCity·",
+					[8] = "Conjured Crystal Water·INV_Drink_18··8079·",
+					[9] = "Teleport: Thunder Bluff·Spell_Arcane_TeleportThunderBluff·",
+					[10] = "Dampen Magic·Spell_Nature_AbolishMagic·5·",
+					[11] = "Amplify Magic·Spell_Holy_FlashHeal·4·",
+					[12] = "Tailoring·Trade_Tailoring·",
+				},
+				[3] = {
+					[1] = "Conjure Water·INV_Drink_18·7·",
+					[2] = "Conjure Food·INV_Misc_Food_73CinnamonRoll·7·",
+					[4] = "Conjure Mana Citrine·INV_Misc_Gem_Opal_01·",
+					[5] = "Conjure Mana Jade·INV_Misc_Gem_Emerald_02·",
+					[6] = "Ice Armor·Spell_Frost_FrostArmor02·4·",
+					[7] = "Portal: Undercity·Spell_Arcane_PortalUnderCity·",
+					[8] = "Portal: Orgrimmar·Spell_Arcane_PortalOrgrimmar·",
+					[9] = "Portal: Thunder Bluff·Spell_Arcane_PortalThunderBluff·",
+					[10] = "Detect Magic·Spell_Holy_Dizzy·",
+					[11] = "Remove Lesser Curse·Spell_Nature_RemoveCurse·",
+					[12] = "Slow Fall·Spell_Magic_FeatherFall·",
+				},
+				[4] = {
+					[1] = "Fire Ward·Spell_Fire_FireArmor·5·",
+					[2] = "Frost Ward·Spell_Frost_FrostWard·4·",
+					[3] = "Mana Jade·INV_Misc_Gem_Emerald_02··?·",
+					[4] = "Mana Citrine·INV_Misc_Gem_Opal_01··?·",
+					[8] = "Bloodcaller·INV_Sword_18··?·",
+					[9] = "Swift Olive Raptor·Ability_Mount_Raptor··18789·",
+					[10] = "Demonic Rune·INV_Misc_Rune_04··?·",
+					[11] = "Major Mana Potion·INV_Potion_76··?·",
+					[12] = "Heavy Runecloth Bandage·INV_Misc_Bandage_12··14530·",
+				},
+				[5] = {
+					[1] = "Blink·Spell_Arcane_Blink·",
+					[2] = "Ice Barrier·Spell_Ice_Lament·4·",
+					[3] = "Ice Block·Spell_Frost_Frost·",
+					[4] = "Cold Snap·Spell_Frost_WizardMark·",
+					[5] = "Frost Nova·Spell_Frost_FrostNova·1·",
+					[6] = "Counterspell·Spell_Frost_IceShock·",
+					[7] = "Evocation·Spell_Nature_Purge·",
+					[8] = "Polymorph·Spell_Nature_Polymorph·4·",
+					[9] = "Shoot·INV_Wand_09·",
+					[11] = "Mana Ruby·INV_Misc_Gem_Ruby_01··8008·",
+					[12] = "Conjure Mana Ruby·INV_Misc_Gem_Ruby_01·",
+				},
+				[6] = {
+				},
+				[7] = {
+				},
+				[8] = {
+				},
+				[9] = {
+				},
+				[0] = {
+					[1] = "Blizzard·Spell_Frost_IceStorm·6·",
+					[2] = "Frostbolt·Spell_Frost_FrostBolt02·11·",
+					[3] = "Fire Blast·Spell_Fire_Fireball·1·",
+					[4] = "Cone of Cold·Spell_Frost_Glacier·5·",
+					[5] = "Arcane Explosion·Spell_Nature_WispSplode·6·",
+					[6] = "Flamestrike·Spell_Fire_SelfDestruct·6·",
+					[7] = "Fireball·Spell_Fire_FlameBolt·12·",
+					[8] = "Blizzard·Spell_Frost_IceStorm·1·",
+					[9] = "Arcane Missiles·Spell_Nature_StarFall·8·",
+					[10] = "Frostbolt·Spell_Frost_FrostBolt02·2·",
+					[11] = "Talisman of Ephemeral Power·INV_Misc_StoneTablet_11··18820·",
+				},
+			},
+		},
+		["MissingItems"] = {
+			[72] = "Zandalarian Hero Charm·INV_Jewelry_Necklace_13··?···Luuga·",
+		},
+		["CurrentSet"] = "Luuga",
+	},
+}
+SAS_BackUp = nil
+SAS_POS = 270
+SAS_OFFSET = 0
