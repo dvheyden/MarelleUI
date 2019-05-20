2cfc2984ce4b4cb01a3e4e90ce107858c5b8fe90
dvheyden <44519345+dvheyden@users.noreply.github.com>
2019-05-20 00:21:08 -0400

    Update pfUI

--- Interface/AddOns/pfUI/api/ui-widgets.lua
+++ Interface/AddOns/pfUI/api/ui-widgets.lua
@@ -552,6 +552,7 @@ function pfUI.api.SkinDropDown(frame, cr, cg, cb)
   local funcc = button:GetScript("OnClick")
   button:SetScript("OnClick", function()
     if funcc then funcc() end
+
     local DropDownListWidth = DropDownList1:GetWidth()
     local DropDownFrameWidth = this:GetParent().backdrop:GetWidth()
     if DropDownListWidth < DropDownFrameWidth then
@@ -561,8 +562,6 @@ function pfUI.api.SkinDropDown(frame, cr, cg, cb)
         _G["DropDownList1Button" .. i]:SetWidth(_G["DropDownList1Button" .. i]:GetWidth() + diff)
       end
     end
-
-    CreateBackdrop(DropDownList1Backdrop, nil, true, .8)
   end)
 
   frame.button = button
